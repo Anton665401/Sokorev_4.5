64726a1978335af97e14ee6dbe6e974b3e131707
Antoxa <Anton.sokorev@mail.ru>
2024-03-11 13:29:41 +0800

    Разрешить конфликты

---
+++ src/registration.svelte
@@ -0,0 +1,34 @@
+   <!-- registration.svelte -->
+   <script>
+       let name = '';
+       let email = '';
+
+       async function registerUser() {
+           const response = await fetch('https://api.example.com/register', {
+               method: 'POST',
+               headers: {
+                   'Content-Type': 'application/json'
+               },
+               body: JSON.stringify({ name, email })
+           });
+
+           if (response.ok) {
+               alert('Пользователь успешно зарегистрирован!');
+           } else {
+               alert('Ошибка при регистрации пользователя');
+           }
+       }
+   </script>
+
+   <h1>Регистрация нового пользователя</h1>
+
+   <form on:submit|preventDefault="{registerUser}">
+       <label for="name">Имя:</label>
+       <input type="text" id="name" bind:value="{name}">
+       
+       <label for="email">Email:</label>
+       <input type="email" id="email" bind:value="{email}">
+       
+       <button type="submit">Зарегистрировать</button>
+   </form>
+   
---
+++ src/users.svelte
@@ -0,0 +1,20 @@
+<!-- users.svelte -->
+   <script>
+       let users = [];
+
+       async function fetchUsers() {
+           const response = await fetch('https://api.example.com/users');
+           const data = await response.json();
+           users = data;
+       }
+
+       fetchUsers();
+   </script>
+
+   <h1>Список пользователей</h1>
+
+   <ul>
+       {#each users as user}
+           <li>{user.name}</li>
+       {/each}
+   </ul>
